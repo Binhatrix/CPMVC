007625b0b3fc7cd5f3df011c60156e0f92c5ec46
binhatrix <rezunho@hotmail.com>
2024-03-21 20:53:59 -0300

    CP - C# RM 99104

---
+++ MVC-RM99104/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_RM99104.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC-RM99104/Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_RM99104.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+    }
+}
